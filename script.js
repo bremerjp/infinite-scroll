6dc7f90de6e908b8473dfc23f7ea2584299156e0
Jacob Bremer <bremerjp@gmail.com>
2020-10-12 16:18:20 -0400

    added layout and API set up

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('cheese');
+// Unsplash API
+const count = 10;
+const apiKey = '9BASqXi9m5KKedWoLf0q6UcIe3pAtXTMoIYNe5XLqv0';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
